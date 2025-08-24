765673dfe6a2f17ad32becbd8813978dfe0be25d
AnushaRumi <121709500+AnushaRumi@users.noreply.github.com>
2025-08-24 23:52:21 +0530

    Add files via upload

---
+++ Auth.js
@@ -0,0 +1,133 @@
+const bcrypt=require("bcryptjs");
+const User=require("../models/User");
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
+
+exports.signup=async (req,res)=>{
+    try{
+        const {name,email,password,role}=req.body;
+        const existingUser=await User.findOne({email});
+
+        if(existingUser){
+            return res.status(400).json({
+            message: 'User already Exists', //  Correct
+     });
+        
+        }
+            let hashedPassword;
+            try{
+                hashedPassword=await bcrypt.hash(password,10);
+
+            }
+            catch(err){
+                return res.status(500).json({
+                    success:false,
+                    message:'Error in hashing Password'
+                });
+            }
+
+           const user=await User.create({
+  		    name,
+            email,
+            password: hashedPassword,
+            role
+         })
+
+            return res.status(200).json({
+                success:true,
+                message:"User Created Successfully",
+            });
+       
+    }
+    catch(error){
+        console.error(error);
+        return res.status(500).json({
+            success:false,
+            message:"User cannot be registered,plz try again later"
+        
+        })
+
+    }
+}
+
+//login
+exports.login = async (req,res) => {
+    try {
+
+        //data fetch
+        const {email, password} = req.body;
+        //validation on email and password
+        if(!email || !password) {
+            return res.status(400).json({
+                success:false,
+                message:'PLease fill all the details carefully',
+            });
+        }
+
+        //check for registered user
+        let user = await User.findOne({email});
+        //if not a registered user
+        if(!user) {
+            return res.status(401).json({
+                success:false,
+                message:'User is not registered',
+            });
+        }
+
+        const payload = {
+            email:user.email,
+            id:user._id,
+            role:user.role,
+        };
+        //verify password & generate a JWT token
+        if(await bcrypt.compare(password,user.password) ) {
+            //password match
+            let token =  jwt.sign(payload, 
+                                process.env.JWT_SECRET,
+                                {
+                                    expiresIn:"2h",
+                                });
+
+                                
+
+            user = user.toObject();
+            user.token = token;
+            user.password = undefined;
+
+            const options = {
+                expires: new Date( Date.now() + 3 * 24 * 60 * 60 * 1000),
+                httpOnly:true,
+            }
+
+               res.cookie("babbarCookie", token, options).status(200).json({
+                   success:true,
+                    token,
+                   user,
+                   message:'User Logged in successfully',
+            });
+
+            //    res.status(200).json({
+            //         success:true,
+            //         token,
+            //        user,
+            //         message:'User Logged in successfully',
+            //    });
+        }
+        else {
+            //passwsord do not match
+            return res.status(403).json({
+                success:false,
+                message:"Password Incorrect",
+            });
+        }
+
+    }
+    catch(error) {
+        console.log(error);
+        return res.status(500).json({
+            success:false,
+            message:'Login Failure',
+        });
+
+    }
+}
